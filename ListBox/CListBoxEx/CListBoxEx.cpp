940398f051a079402b3b5f484c421a5b487b0076
Pennywise007 <s-kacnep@ya.ru>
2022-07-13 12:56:17 +0300

    Add custom draw function + improve performance

--- ListBox/CListBoxEx/CListBoxEx.cpp
+++ ListBox/CListBoxEx/CListBoxEx.cpp
@@ -1,14 +1,22 @@
 #include "Controls/ThemeManagement.h"
 
+#include <afxcontrolbarutil.h>
+#include <algorithm>
+#include <tuple>
+
 #include "CListBoxEx.h"
 
 BEGIN_MESSAGE_MAP(CListBoxEx, CListBox)
-    ON_WM_MEASUREITEM_REFLECT()
     ON_WM_SIZE()
+    ON_WM_PAINT()
+    ON_WM_ERASEBKGND()
+    ON_WM_MEASUREITEM_REFLECT()
 END_MESSAGE_MAP()
 
-////////////////////////////////////////////////////////////////////////////////
-// CListBoxEx
+CListBoxEx::CListBoxEx(bool multilineText /*= true*/)
+    : m_multilineText(multilineText)
+{}
+
 int CListBoxEx::AddItem(const CString& itemText, COLORREF color, int nIndex /*= -1*/)
 {
     // индекс куда вставл€ем элемент
@@ -19,7 +27,7 @@ int CListBoxEx::AddItem(const CString& itemText, COLORREF color, int nIndex /*= 
     else
         index = InsertString(nIndex, itemText);
 
-    SetItemData(index,color);
+    m_colorsByLines[index] = color;
 
     // иногда при добавлении элементов не возникает перерисовки
     Invalidate();
@@ -27,12 +35,10 @@ int CListBoxEx::AddItem(const CString& itemText, COLORREF color, int nIndex /*= 
     return index;
 }
 
-//----------------------------------------------------------------------------//
 void CListBoxEx::PreSubclassWindow()
 {
     // провер€ем необходимые стили, их невозможно установить через ModifyStyle(не будут работать)
-    if (!(CListBox::GetStyle() & LBS_HASSTRINGS) ||
-        !(CListBox::GetStyle() & LBS_OWNERDRAWVARIABLE))
+    if (m_multilineText && (!(CListBox::GetStyle() & LBS_HASSTRINGS | LBS_OWNERDRAWVARIABLE)))
         ASSERT(!"” списка должен быть установлен стиль LBS_OWNERDRAWVARIABLE и LBS_HASSTRINGS дл€ корректной работы!");
 
     EnableWindowTheme(GetSafeHwnd(), L"ListBox", L"Explorer", NULL);
@@ -40,11 +46,10 @@ void CListBoxEx::PreSubclassWindow()
     CListBox::PreSubclassWindow();
 }
 
-//----------------------------------------------------------------------------//
 void CListBoxEx::MeasureItem(LPMEASUREITEMSTRUCT lpMeasureItemStruct)
 {
     // „тобы вызывалс€ нужен стиль LBS_OWNERDRAWVARIABLE
-    int nItem = lpMeasureItemStruct->itemID;
+    const int nItem = lpMeasureItemStruct->itemID;
     CString sLabel;
     CRect rcLabel;
 
@@ -58,16 +63,36 @@ void CListBoxEx::MeasureItem(LPMEASUREITEMSTRUCT lpMeasureItemStruct)
     lpMeasureItemStruct->itemHeight = dc.DrawText(sLabel, -1, rcLabel, DT_WORDBREAK | DT_CALCRECT);
 }
 
-//----------------------------------------------------------------------------//
 void CListBoxEx::DrawItem(LPDRAWITEMSTRUCT lpDIS)
 {
     // ѕри клике в пустую область может придти DrawItem с невалидным идентификеатором
     if (lpDIS->itemID == -1)
         return;
 
-    CDC* pDC = CDC::FromHandle(lpDIS->hDC);
+    const auto getColors = [](HWND hwnd)
+    {
+        HTHEME hTheme = ::OpenThemeData(hwnd, L"LISTBOX");
+        ASSERT(hTheme);
 
-    COLORREF rColor = (COLORREF)lpDIS->itemData; // RGB in item data
+        const COLORREF highLightColor = ::GetThemeSysColor(hTheme, COLOR_HIGHLIGHT);
+        const COLORREF highLightTextColor = ::GetThemeSysColor(hTheme, COLOR_HIGHLIGHTTEXT);
+        ::CloseThemeData(hTheme);
+
+        return std::make_tuple(highLightColor, highLightTextColor);
+    };
+    const static auto colors = getColors(m_hWnd);
+    const COLORREF highLightColor = std::get<0>(colors);
+    const COLORREF highLightTextColor = std::get<1>(colors);
+
+    CDC& dc = *CDC::FromHandle(lpDIS->hDC);
+
+    const COLORREF backColor = [&]()
+    {
+        const auto it = m_colorsByLines.find(lpDIS->itemID);
+        if (it == m_colorsByLines.end())
+            return dc.GetBkColor();
+        return it->second;
+    }();
 
     CString sLabel;
     GetText(lpDIS->itemID, sLabel);
@@ -76,94 +101,161 @@ void CListBoxEx::DrawItem(LPDRAWITEMSTRUCT lpDIS)
     if ((lpDIS->itemState & ODS_SELECTED) &&
         (lpDIS->itemAction & (ODA_SELECT | ODA_DRAWENTIRE)))
     {
-        // draw color box
-        CBrush colorBrush(rColor);
-        CRect colorRect = lpDIS->rcItem;
-
-        HTHEME hTheme = OpenThemeData(m_hWnd, L"LISTBOX");
-
         // draw label background
-        CBrush labelBrush(::GetThemeSysColor(hTheme, COLOR_HIGHLIGHT));
+        CBrush labelBrush(highLightColor);
         CRect labelRect = lpDIS->rcItem;
-        pDC->FillRect(&labelRect,&labelBrush);
+        dc.FillRect(&labelRect,&labelBrush);
 
         // draw label text
-        COLORREF colorTextSave = pDC->SetTextColor(::GetThemeSysColor(hTheme, COLOR_HIGHLIGHTTEXT));
-        COLORREF colorBkSave = pDC->SetBkColor(::GetThemeSysColor(hTheme, COLOR_HIGHLIGHT));
-        pDC->DrawText( sLabel, -1, &lpDIS->rcItem, DT_WORDBREAK);
+        COLORREF colorTextSave = dc.SetTextColor(highLightTextColor);
+        COLORREF colorBkSave = dc.SetBkColor(highLightColor);
+        dc.DrawText( sLabel, -1, &lpDIS->rcItem, m_multilineText ? DT_WORDBREAK : 0);
 
-        pDC->SetTextColor(colorTextSave);
-        pDC->SetBkColor(colorBkSave);
-
-        // прекращаем работу с темой
-        if (hTheme)
-            CloseThemeData(hTheme);
-
-        return;
+        dc.SetTextColor(colorTextSave);
+        dc.SetBkColor(colorBkSave);
     }
-
     // item brought into box
-    if (lpDIS->itemAction & ODA_DRAWENTIRE)
+    else if (lpDIS->itemAction & ODA_DRAWENTIRE)
     {
-        CBrush brush(rColor);
+        CBrush brush(backColor);
         CRect rect = lpDIS->rcItem;
-        pDC->SetBkColor(rColor);
-        pDC->FillRect(&rect,&brush);
-        pDC->DrawText( sLabel, -1, &lpDIS->rcItem, DT_WORDBREAK);
-
-        return;
+        dc.SetBkColor(backColor);
+        dc.FillRect(&rect,&brush);
+        dc.DrawText( sLabel, -1, &lpDIS->rcItem, m_multilineText ? DT_WORDBREAK : 0);
     }
-
     // item deselected
-    if (!(lpDIS->itemState & ODS_SELECTED) &&
+    else if (!(lpDIS->itemState & ODS_SELECTED) &&
         (lpDIS->itemAction & ODA_SELECT))
     {
         CRect rect = lpDIS->rcItem;
-        CBrush brush(rColor);
-        pDC->SetBkColor(rColor);
-        pDC->FillRect(&rect,&brush);
-        pDC->DrawText( sLabel, -1, &lpDIS->rcItem, DT_WORDBREAK);
-
-        return;
+        CBrush brush(backColor);
+        dc.SetBkColor(backColor);
+        dc.FillRect(&rect,&brush);
+        dc.DrawText( sLabel, -1, &lpDIS->rcItem, m_multilineText ? DT_WORDBREAK : 0);
     }
 }
 
-//----------------------------------------------------------------------------//
 void CListBoxEx::OnSize(UINT nType, int cx, int cy)
 {
-    MEASUREITEMSTRUCT measureStruct;
-
-    // дл€ каждого элемента пересчитываем размер
-    for (int item = 0, count = CListBox::GetCount();
-         item < count; ++item)
+    if (m_multilineText)
     {
-        // смотрим сколько он занимает по высоте
-        measureStruct.itemID = item;
-        MeasureItem(&measureStruct);
+        MEASUREITEMSTRUCT measureStruct;
 
-        // если высота изменилась надо еЄ переустановить
-        if (GetItemHeight(item) != measureStruct.itemHeight)
-            SetItemHeight(item, measureStruct.itemHeight);
+        // дл€ каждого элемента пересчитываем размер
+        for (int item = 0, count = CListBox::GetCount();
+             item < count; ++item)
+        {
+            // смотрим сколько он занимает по высоте
+            measureStruct.itemID = item;
+            MeasureItem(&measureStruct);
+
+            // если высота изменилась надо еЄ переустановить
+            if (GetItemHeight(item) != measureStruct.itemHeight)
+                SetItemHeight(item, measureStruct.itemHeight);
+        }
+
+        // иногда возникают случаи(при показе скроллера) когда не происходит перерисовка
+        Invalidate();
     }
-
-    // иногда возникают случаи(при показе скроллера) когда не происходит перерисовка
-    Invalidate();
 
     CListBox::OnSize(nType, cx, cy);
 }
 
-//----------------------------------------------------------------------------//
+BOOL CListBoxEx::OnEraseBkgnd(CDC* pDC)
+{
+    return TRUE;
+}
+
 LRESULT CListBoxEx::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
 {
-    if (message == LB_ADDSTRING || message == LB_INSERTSTRING)
-    {
-        LRESULT res = CListBox::WindowProc(message, wParam, lParam);
-
-        // ≈сли добавл€етс€ строка надо ей задать данные с нормальным цветом
-        SetItemData((int)res, GetBkColor(GetDC()->m_hDC));
-
-        return res;
-    }
+    if (message == LB_RESETCONTENT)
+        m_colorsByLines.clear();
 
     return CListBox::WindowProc(message, wParam, lParam);
+}
+
+void CListBoxEx::OnPaint()
+{
+    CRect drawRect;
+    CListBox::GetClientRect(drawRect);
+
+    if (drawRect.IsRectEmpty())
+        return;
+
+    CPaintDC dcPaint(this);
+    CMemDC memDC(dcPaint, drawRect);
+    CDC& dc = memDC.GetDC();
+
+    dc.SaveDC();
+    dc.SetWindowOrg(GetScrollPos(SB_HORZ), 0);
+
+    dc.SelectObject(CListBox::GetFont());
+
+    {
+        int minPos = 0, maxPos = 0;
+        GetScrollRange(SB_HORZ, &minPos, &maxPos);
+
+        CRect visibleRect = drawRect;
+        visibleRect.right = std::max<LONG>(visibleRect.right, maxPos);
+        dc.FillSolidRect(&visibleRect, GetBkColor(dc.m_hDC));
+    }
+
+    const auto getItemUnderCursor = [&]()
+    {
+        CPoint cursor;
+        ::GetCursorPos(&cursor);
+        CListBox::ScreenToClient(&cursor);
+        BOOL itemUnderCursor;
+        int cursorItem = ItemFromPoint(cursor, itemUnderCursor);
+        if (itemUnderCursor == FALSE)
+            cursorItem = -1;
+        return cursorItem;
+    };
+    const int itemUnderCursor = getItemUnderCursor();
+    const bool focused = CListBox::GetFocus() == this;
+    const int countItems = CListBox::GetCount();
+    if (countItems == 0)
+    {
+        dc.RestoreDC(-1);
+        return;
+    }
+
+    DRAWITEMSTRUCT drawStruct;
+    drawStruct.CtlType = ODT_LISTBOX;
+    drawStruct.CtlID = CListBox::GetDlgCtrlID();
+    drawStruct.hwndItem = m_hWnd;
+    drawStruct.hDC = dc.m_hDC;
+    drawStruct.itemAction = ODA_DRAWENTIRE;
+    drawStruct.itemState = CListBox::IsWindowEnabled() == FALSE ? ODS_DISABLED : 0;
+    drawStruct.itemID = CListBox::GetTopIndex();
+
+    do
+    {
+        drawStruct.itemData = CListBox::GetItemData(drawStruct.itemID);
+        CListBox::GetItemRect(drawStruct.itemID, &drawStruct.rcItem);
+
+        const auto prevAction = drawStruct.itemAction;
+        const auto prevState = drawStruct.itemState;
+
+        const bool selected = CListBox::GetSel(drawStruct.itemID) > 0;
+        if (selected)
+        {
+            drawStruct.itemAction |= ODA_SELECT | (focused ? ODS_FOCUS : ODS_NOFOCUSRECT);
+            drawStruct.itemState |= ODS_SELECTED;
+        }
+        else
+            drawStruct.itemAction |= ODS_DEFAULT;
+
+        if (itemUnderCursor)
+            drawStruct.itemState |= ODS_HOTLIGHT;
+
+        DrawItem(&drawStruct);
+
+        drawStruct.itemAction = prevAction;
+        drawStruct.itemState = prevState;
+        if (++drawStruct.itemID == countItems)
+            break;
+    } while (drawStruct.rcItem.bottom <= drawRect.bottom);
+
+    dc.RestoreDC(-1);
 }
--- ListBox/CListBoxEx/CListBoxEx.h
+++ ListBox/CListBoxEx/CListBoxEx.h
@@ -2,16 +2,15 @@
 
 // Класс обёртка над CListBox, позволяет устанавливать цвета элементам и рисовать их в несколько строк
 #include <afxwin.h>
+#include <unordered_map>
 
 ////////////////////////////////////////////////////////////////////////////////
 // CListBoxEx
 class CListBoxEx : public CListBox
 {
 public:
-    CListBoxEx()
-    {
+    CListBoxEx(bool multilineText = true);
 
-    }
     // Вставить элемент с заданием цвета
     // если nIndex == -1 делается AddString иначе - вставляется по указанному индексу
     int AddItem(const CString& itemText, COLORREF color, int nIndex = -1);
@@ -24,4 +23,10 @@ protected:
     virtual LRESULT WindowProc(UINT message, WPARAM wParam, LPARAM lParam) override;
     afx_msg void MeasureItem(LPMEASUREITEMSTRUCT lpMeasureItemStruct);
     afx_msg void OnSize(UINT nType, int cx, int cy);
+    afx_msg void OnPaint();
+    BOOL OnEraseBkgnd(CDC* pDC);
+
+private:
+    const bool m_multilineText;
+    std::unordered_map<int, COLORREF> m_colorsByLines;
 };
