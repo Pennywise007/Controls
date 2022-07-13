9b53ff8add1028dfd1e9e91b32fcf3429fbeff65
Pennywise007 <s-kacnep@ya.ru>
2022-07-13 13:00:43 +0300

    Adding custom control for removing flickering

---
+++ MFC/CMFCEditBrowseCtrlEx.cpp
@@ -0,0 +1,10 @@
+﻿#include "CMFCEditBrowseCtrlEx.h"
+
+BEGIN_MESSAGE_MAP(CMFCEditBrowseCtrlEx, CMFCEditBrowseCtrl)
+END_MESSAGE_MAP()
+
+void CMFCEditBrowseCtrlEx::OnDrawBrowseButton(CDC* pDC, CRect rect, BOOL bIsButtonPressed, BOOL bIsButtonHot)
+{
+    CMemDC dc(*pDC, rect);
+    __super::OnDrawBrowseButton(&dc.GetDC(), rect, bIsButtonPressed, bIsButtonHot);
+}
---
+++ MFC/CMFCEditBrowseCtrlEx.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <afx.h>
+#include <ShlObj_core.h>
+#include <afxeditbrowsectrl.h>
+
+class CMFCEditBrowseCtrlEx : public CMFCEditBrowseCtrl
+{
+protected:
+    DECLARE_MESSAGE_MAP()
+
+    virtual void OnDrawBrowseButton(CDC* pDC, CRect rect, BOOL bIsButtonPressed, BOOL bIsButtonHot) override;
+};
