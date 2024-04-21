f4b6605cc380f80187ea24a330aa5da93273ff1e
Pennywise007 <skacnep@gmail.com>
2024-04-21 20:08:29 +0200

    Adding all windows messages callbacks Fixing scenarious when callbacks got unsubscribed during call execution

--- DefaultWindowProc.h
+++ DefaultWindowProc.h
@@ -67,7 +67,7 @@ public:
         if (messagesCallbacksIt->second.empty())
         {
             windowInfoIt->second.callbacksByMessages.erase(messagesCallbacksIt);
-            if (windowInfoIt->second.callbacksByMessages.empty())
+            if (windowInfoIt->second.callbacksByMessages.empty() && windowInfoIt->second.anyMessagesCallbacks.empty())
             {
                 // restore default window proc
                 ::SetWindowLongPtr(targetWindow, GWLP_WNDPROC, (LONG_PTR)windowInfoIt->second.defaultWindowProc);
@@ -75,6 +75,53 @@ public:
             }
         }
     }
+
+    /// Callback for handling message from window, set result if you want to interrupt handling message by other handlers
+    typedef std::function<void(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam, LRESULT& result)> AnyMessageCallback;
+    /// Add callback on window message
+    static void OnAnyWindowMessage(const CWnd& targetWindow, AnyMessageCallback&& callback, const LPVOID handler)
+    {
+        ENSURE(::IsWindow(targetWindow));
+        ENSURE(!!handler);
+
+        DefaultWindowProc& instance = Instance();
+        instance.TryAttachToWindowProc(targetWindow);
+
+        auto windowInfoIt = instance.m_windowsInfo.find(static_cast<HWND>(targetWindow));
+        ASSERT(windowInfoIt != instance.m_windowsInfo.end());
+        windowInfoIt->second.anyMessagesCallbacks.emplace(handler, std::move(callback));
+    }
+    /// Add callback on window message
+    static void RemoveAnyMessageCallback(const CWnd& targetWindow, const LPVOID handler)
+    {
+        DefaultWindowProc& instance = Instance();
+
+        auto windowInfoIt = instance.m_windowsInfo.find(static_cast<HWND>(targetWindow));
+        if (windowInfoIt == instance.m_windowsInfo.end())
+        {
+            // no callbacks for target window
+            return;
+        }
+
+        auto handlerCallbackIt = windowInfoIt->second.anyMessagesCallbacks.find(handler);
+        if (handlerCallbackIt == windowInfoIt->second.anyMessagesCallbacks.end())
+        {
+            // this handler hasn`t subscribed to events yet
+            ASSERT(FALSE);
+            return;
+        }
+
+        // clean callbacks
+        windowInfoIt->second.anyMessagesCallbacks.erase(handlerCallbackIt);
+        if (windowInfoIt->second.callbacksByMessages.empty() && windowInfoIt->second.anyMessagesCallbacks.empty())
+        {
+            // restore default window proc
+            ::SetWindowLongPtr(targetWindow, GWLP_WNDPROC, (LONG_PTR)windowInfoIt->second.defaultWindowProc);
+            instance.m_windowsInfo.erase(windowInfoIt);
+        }
+    }
+
+
 private:
     // allow creating only from instance
     DefaultWindowProc() = default;
@@ -107,7 +154,8 @@ private:
     // if we don`t need to handle def window proc but we do - detach
     void CheckNecessityToHandleDefProc(HWND hWnd)
     {
-        if (const auto it = m_windowsInfo.find(hWnd); it != m_windowsInfo.end() && it->second.callbacksByMessages.empty())
+        if (const auto it = m_windowsInfo.find(hWnd); it != m_windowsInfo.end() &&
+            it->second.callbacksByMessages.empty() && it->second.anyMessagesCallbacks.empty())
         {
             ::SetWindowLongPtr(hWnd, GWLP_WNDPROC, (LONG_PTR)it->second.defaultWindowProc);
             m_windowsInfo.erase(it);
@@ -117,44 +165,83 @@ private:
     static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
         DefaultWindowProc& instance = Instance();
-        const auto windowInfoIt = instance.m_windowsInfo.find(hWnd);
+        auto windowInfoIt = instance.m_windowsInfo.find(hWnd);
         if (windowInfoIt == instance.m_windowsInfo.end())
         {
             ASSERT(FALSE);
             return S_FALSE;
         }
 
-        const CallbacksForMessage& callbacks = windowInfoIt->second.callbacksByMessages;
-        const auto messageCallbackIt = callbacks.find(uMsg);
-        if (messageCallbackIt != callbacks.end())
+#ifdef DEBUG // debugging helper
+        CString str;
+        if (uMsg != WM_GETTEXT && uMsg != WM_GETTEXTLENGTH)
         {
-#ifdef DEBUG
-            CString str;
             if (::IsWindow(hWnd))
                 CWnd::FromHandle(hWnd)->GetWindowText(str);
             else
                 ASSERT(FALSE);
+        }
 #endif
+        WNDPROC defaultWindowProc = windowInfoIt->second.defaultWindowProc;
 
-            LRESULT result = 0;
-            for (auto&& [handler, callback] : messageCallbackIt->second)
+        LRESULT result = 0;
+        for (auto&& [handler, callback] : windowInfoIt->second.anyMessagesCallbacks)
+        {
+            callback(hWnd, uMsg, wParam, lParam, result);
+
+            // callback might unsubscribe itself
+            windowInfoIt = instance.m_windowsInfo.find(hWnd);
+            if (windowInfoIt == instance.m_windowsInfo.end())
             {
-                callback(hWnd, wParam, lParam, result);
-                if (result)
+                if (result || !::IsWindow(hWnd))
+                    return 1;
+                return ::CallWindowProc(defaultWindowProc, hWnd, uMsg, wParam, lParam);
+            }
+
+            if (result)
+            {
+                if (uMsg == WM_NCDESTROY)
+                    break;
+
+                return result;
+            }
+        }
+
+        if (!result)
+        {
+            const CallbacksForMessage& callbacks = windowInfoIt->second.callbacksByMessages;
+            const auto messageCallbackIt = callbacks.find(uMsg);
+            if (messageCallbackIt != callbacks.end())
+            {
+                for (auto&& [handler, callback] : messageCallbackIt->second)
                 {
-                    if (uMsg == WM_NCDESTROY)
-                        break;
-                    return result;
+                    callback(hWnd, wParam, lParam, result);
+
+                    // callback might unsubscribe itself
+                    windowInfoIt = instance.m_windowsInfo.find(hWnd);
+                    if (windowInfoIt == instance.m_windowsInfo.end())
+                    {
+                        if (result || !::IsWindow(hWnd))
+                            return 1;
+                        return ::CallWindowProc(defaultWindowProc, hWnd, uMsg, wParam, lParam);
+                    }
+
+                    if (result)
+                    {
+                        if (uMsg == WM_NCDESTROY)
+                            break;
+                        return result;
+                    }
                 }
             }
         }
 
-        WNDPROC defaultWindowProc = windowInfoIt->second.defaultWindowProc;
         switch (uMsg)
         {
         case WM_NCDESTROY:
             // restore def window proc
             ::SetWindowLongPtr(hWnd, GWLP_WNDPROC, (LONG_PTR)defaultWindowProc);
+            // don't use 
             instance.m_windowsInfo.erase(windowInfoIt);
             break;
         default:
@@ -165,6 +252,7 @@ private:
     }
 
 private:
+    typedef std::unordered_map<LPVOID, AnyMessageCallback> AnyMessageCallbackByHandler;
     typedef std::unordered_map<LPVOID, Callback> CallbackByHandler;
     typedef std::unordered_map<UINT, CallbackByHandler> CallbacksForMessage;
     struct WindowInfo
@@ -172,6 +260,8 @@ private:
         explicit WindowInfo(WNDPROC proc) noexcept : defaultWindowProc(proc) {}
 
         WNDPROC defaultWindowProc;
+
+        AnyMessageCallbackByHandler anyMessagesCallbacks;
         CallbacksForMessage callbacksByMessages;
     };
 
