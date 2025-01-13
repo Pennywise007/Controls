390eebfa26b1ae22e20d18232a3fc46198481815
Pennywise <s-kacnep@ya.ru>
2025-01-13 12:06:46 +0100

    Fixing high DPI mode picture

--- MFC/CMFCEditBrowseCtrlEx.h
+++ MFC/CMFCEditBrowseCtrlEx.h
@@ -8,6 +8,7 @@ class CMFCEditBrowseCtrlEx : public CMFCEditBrowseCtrl
 {
 protected:
     DECLARE_MESSAGE_MAP()
-
+        
+    afx_msg LRESULT OnInitControl(WPARAM wParam, LPARAM lParam);
     virtual void OnDrawBrowseButton(CDC* pDC, CRect rect, BOOL bIsButtonPressed, BOOL bIsButtonHot) override;
 };
